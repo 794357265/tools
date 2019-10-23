2fe320b3999644ffb4ea36370269fa099f44cc89
794357265@qq.com <xxx0.618>
2019-10-23 17:36:19 +0800

    2019/10/23 17:35

---
+++ src/cn/net/zhangyibing/main/Main.java
@@ -0,0 +1,12 @@
+package cn.net.zhangyibing.main;
+
+import cn.net.zhangyibing.encrypt.utils.Base64Utils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Base64Utils.base64Encrypt("123");
+    }
+
+}
